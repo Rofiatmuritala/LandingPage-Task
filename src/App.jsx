09fe6aedf9a86767a9471be72aa1eb856ea7c315
rofiatmuritala <rofiatmuritalaa@gmail.com>
2024-03-20 13:48:17 +0000

    Merge branch 'main' of https://github.com/Rofiatmuritala/LandingPage-Task

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-import Shop from "./components/Shop";
-import Contact from "./components/contact";
+import HomePage from "./page/HomePage";
+import ContactPage from "./page/ContactPage";
 
 function App() {
   return (
     <>
-      
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
